b51d45849304baaa73fb7379c5aff90ca617aa23
Karina Custódio Sousa <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-03-29 17:31:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,25 @@ conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
 planilha = api.open_by_key("1BTcO4G_FS1tp6_hRcPUk_4fts6ayt7Ms2cvYHsqD9nM")
 sheet = planilha.worksheet("dadosrobo")
+
+#Criando função que baixa arquivo
+def baixar_arquivo(url, endereco):
+    resposta = requests.get(url)
+    if resposta.status_code == requests.codes.OK:
+        with open(endereco, 'wb') as novo_arquivo:
+            novo_arquivo.write(resposta.content)
+        print("Donwload finalizado. Salvo em: {}".format(endereco))
+    else:
+        resposta.raise_for_status()
+        
+#baixando arquivo despesas
+baixar_arquivo('https://www.camara.leg.br/cotas/Ano-2022.csv.zip','CSV')
+
+#lendo arquivo  
+with zipfile.ZipFile('CSV') as z:
+  with z.open('Ano-2022.csv') as f:
+    despesas = pd.read_csv(f, sep=';', skiprows = [i for i in range(1, 515) ])
+
 app = Flask(__name__)
 
 @app.route("/")
