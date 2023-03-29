8efd15e7da446fc9c18af6f9acd70e94e21a2760
Karina Custódio Sousa <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-03-29 17:05:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask
+import os
 
+import gspread
+import requests
+from flask import Flask, request
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import zipfile
+import altair as alt
+import plotly.express as px
+
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1BTcO4G_FS1tp6_hRcPUk_4fts6ayt7Ms2cvYHsqD9nM")
+sheet = planilha.worksheet("dadosrobo")
 app = Flask(__name__)
 
 @app.route("/")
