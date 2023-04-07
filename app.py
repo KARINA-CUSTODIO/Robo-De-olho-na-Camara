97da2683421c1bb7343d67951ab06ebaf4ec19be
Karina Custódio Sousa <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-04-07 15:56:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -142,6 +142,7 @@ def telegram_bot():
   nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
  
   resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  print(resposta.text)
   return "ok"
 
 @app.route("/")
