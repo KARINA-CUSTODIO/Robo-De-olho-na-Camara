d3b2be0dbf0a0e47ed2f31f8e00ec996ff1aacde
Karina Custódio Sousa <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-04-08 11:08:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ gastadores = gastadores.sort_values(by='vlrLiquido')
 gastadores = gastadores.reset_index()
 menorgastador = gastadores.iloc[0]['txNomeParlamentar']
 
+#Levando dados do dataframe, pro Google sheets
+sheet_gastadores = planilha.get_worksheet(2)
+sheet_gastadores.update([gastadores.columns.values.tolist()] + gastadores.values.tolist())
+
 #Qual a média de gastos por deputado/a?
 mediaBr = despesas['vlrLiquido'].mean()
 mediaBr = "{:.2f}".format(mediaBr)
@@ -73,6 +77,10 @@ autores = proposicoes.groupby('nomeAutor').count()
 autores = pd.DataFrame(autores)
 autores = autores.sort_values(by='idProposicao', ascending = False)
 autores = autores.reset_index()
+
+#Levando dados do dataframe, pro Google sheets
+sheet_autores = planilha.get_worksheet(2)
+sheet_autores.update([autores.columns.values.tolist()] + autores.values.tolist())
 
 maior_autor = autores.iloc[0]['nomeAutor']
 
@@ -139,23 +147,30 @@ def telegram_bot():
   sender_id = update["message"]["from"]["id"]
 
   # Define qual será a resposta e enviada
+def telegram_bot():
+  #extraindo dados para enviar mensagens
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"].split
+  first_name = update["message"]["from"]["first_name"]
+  sender_id = update["message"]["from"]["id"]
 
-  sheet.update('A1',[message])
-  historico = sheet.row_values(-1)
-
+  # Define qual será a resposta e enviada
   mensagens = ['oi', 'Oi', 'Olá', 'olá', 'ola', 'iai', 'qual é', 'e aí', "/start" ]
   if message in mensagens:
-    texto_resposta = f"Olá! Seja bem-vinda(o) {first_name}! O que gostaria de saber? \n 3 - Gastos de um(a) deputado(a) no último ano? \n 4 - Quantidade de PLs apresentados por um(a) deputado(a) no último ano?"
-  elif message == '3' or '4':
-    texto_resposta = f'{first_name} Digite o nome do(a) parlamentar'
-  elif historico in gastadores:
-    filtro = gastadores.query("txNomeParlamentar == message")
-    gasto = filtro.get('vlrLiquido')
+    texto_resposta = f"Olá! Seja bem-vinda(o) {first_name}! Eu sou o robô de olho na Câmara, para saber o gasto e os Projetos de Lei de um(a) deputado(a) digite seu nome." 
+  elif message in sheet_gastadores:
+    filtro = sheet_gastadores.find(message)
+    gasto = filtro.find('vlrLiquido')
     texto_resposta = f'{first_name} o gasto de {message} foi igual a {gasto}'
-  elif historico in autores:
-    filtro = autores.query("nomeAutor == message")
-    Pls = filtro.get('idProposicao')
+  elif message in sheet_autores:
+    filtro = sheet_autores.find(message)
+    Pls = filtro.find('idProposicao')
     texto_resposta = f'{first_name} {message} apresentou {PLs} no último ano'
+
+    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+    requests.post(f"https://api.telegram.org./bot{token}/sendMessage", data=nova_mensagem)
+    return resultado
 
     nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
     resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
